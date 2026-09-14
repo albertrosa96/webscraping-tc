37163d63fbcc5df7f6c83b9b3a135163595a62f3
albertrosa96 <albertrosa96@gmail.com>
2026-09-14 10:11:34 -0400

    Update ejemplo_actualizacion_manual.R

--- dev/ejemplo_actualizacion_manual.R
+++ dev/ejemplo_actualizacion_manual.R
@@ -3,7 +3,7 @@ source("scripts/read_tc_data.R")
 data <- read_tc_data("banks")
 tail(data)
 
-update_daily_tasa("2026-09-10", "Banco Popular", compra = 55.9, venta = 59.9)
+update_daily_tasa("2026-09-14", "Banco Popular", compra = 56.05, venta = 60.05)
 
 data <- read_tc_data("banks")
 tail(data)
